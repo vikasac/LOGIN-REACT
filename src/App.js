ec759eacc32203a4bd597fa98f4027b28c4d2f75
vikasac <vikasac983@gmail.com>
2024-01-27 12:29:00 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-
-function App() {
+const AdminPanel = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <div className="row">
+        <div className="col-lg-3 col-md-2"></div>
+        <div className="col-lg-6 col-md-8 login-box">
+          <div className="col-lg-12 login-key">
+            <i className="fa fa-key" aria-hidden="true"></i>
+          </div>
+          <div className="col-lg-12 login-title">
+            LOGIN PAGE
+          </div>
+
+          <div className="col-lg-12 login-form">
+            <div className="col-lg-12 login-form">
+              <form>
+                <div className="form-group">
+                  <label className="form-control-label">USERNAME</label>
+                  <input type="text" className="form-control" />
+                </div>
+                <div className="form-group">
+                  <label className="form-control-label">PASSWORD</label>
+                  <input type="password" className="form-control" />
+                </div>
+
+                <div className="col-lg-12 loginbttm">
+                  <div className="col-lg-6 login-btm login-text">
+                    {/* Error Message */}
+                  </div>
+                  <div className="col-lg-6 login-btm login-button">
+                    <button type="submit" className="btn btn-outline-primary">LOGIN</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
+          <div className="col-lg-3 col-md-2"></div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
-export default App;
+export default AdminPanel;
